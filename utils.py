89bf529258d6f8931a918287a91aa4a1acdc9d2a
apy2017 <Anaconda_python@mail.ru>
2017-04-05 21:15:45 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-import shelve # shelve позволяет сериализовать объект, а потом сохранить его в виде похожем на БД 
-from SQLighter import SQLighter
-from config import shelve_name, database_name
-
-# место для функций
