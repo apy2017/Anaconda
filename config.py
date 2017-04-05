3231f42d6dec109f3824179dbd520f3bac6b148c
KateLiliy <kateliliy@mail.ru>
2017-04-05 21:22:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-token = 'TOKEN'  # Токен бота
+token = '342067339:AAFsQBcm8NJjUx8RtJBIlusAyREpSImf0PU'  # Токен бота  
 database_name = 'BD.db'  # Файл с базой данных, если потребуется
 shelve_name = 'shelve.db'  # Файл с хранилищем, если потребуется
