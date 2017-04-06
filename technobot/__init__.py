bffd128be3a8b7bebb36ba6a3a23242ac39361ab
Slava <slovaricheg@gmail.com>
2017-04-06 19:51:43 +0300

    Now it works

--- technobot/__init__.py
+++ technobot/__init__.py
@@ -4,30 +4,34 @@ from threading import Timer
 from technobot import database
 
 class TechnoBot:
-    def __init__(self, token, timeout=3):
+        def __init__(self, token, timeout=3):
         self.token = token
         self._timeout = timeout
+        self._last_update_id = None
         self._updates = {}
-        self._processed_updates = []
         self._thread = Timer(self._timeout, self._process_updates)
 
     def _receive_updates(self):
-        updates = api.get_updates(self.token)
+        self._updates = {}
+        updates = api.get_updates(self.token, self._last_update_id)
         for update in updates:
             new_update = utils.Update(update)
-            self._updates[new_update.update_id] = new_update
+            if self._last_update_id is not None and new_update.update_id > self._last_update_id:
+                self._last_update_id = new_update.update_id
+                self._updates[new_update.update_id] = new_update
+            elif self._last_update_id is None:
+                self._last_update_id = new_update.update_id
+                self._updates[new_update.update_id] = new_update
 
     def _process_updates(self):
         self._receive_updates()
         self._thread.cancel()
         for update_id, update in self._updates.items():
-            if update.message is not None and update_id not in self._processed_updates:
+            if update.message is not None:
                 message = update.message
                 user = update.message.from_user
                 database.db_register_user(user.id, user.first_name, user.last_name)
                 self.send_message(message.chat.id, message.text)
-                # Mark update is processed
-                self._processed_updates.append(update_id)
         self._thread = Timer(self._timeout, self._process_updates)
         self._thread.start()
 
